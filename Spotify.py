4c9cb0dd030e8aba678b844b032d075def570ea0
Moisés <medronic.games@gmail.com>
2022-03-25 17:39:31 -0300

    First commit

---
+++ Spotify.py
@@ -0,0 +1,13 @@
+import time
+import requests
+
+# Spotify API
+
+url_base = "https://api.spotify.com/v1/"
+
+class Spotify:
+    """
+        
+    """
+    def __init__(self, ): #-> None:
+        print(1)
